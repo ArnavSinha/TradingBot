4b2c8424b08152bc3498f8ef9bb3835ba88c2506
Arnav Sinha <sinha.arnav1407@gmail.com>
2020-08-04 00:04:37 +0530

    Initial code - fixed errors

--- tradingbot/bot.py
+++ tradingbot/bot.py
@@ -9,12 +9,12 @@ from typing import List, Dict, Union
 
 class TradingBot():
 
-    def __init__(self, client_id: str, redirect_uri: str, credientials_path: str = None, trading_account: str = None) -> None:
+    def __init__(self, client_id: str, redirect_uri: str, credentials_path: str = None, trading_account: str = None) -> None:
 
         self.trading_account: str = trading_account
         self.client_id: str = client_id
         self.redirect_uri: str = redirect_uri
-        self.credientials_path: str = credientials_path
+        self.credentials_path: str = credentials_path
         self.session: TDClient = self._create_session()
         self.trades: dict = {}
         self.historical_prices: dict = {}
@@ -23,7 +23,7 @@ class TradingBot():
     def _create_session(self) -> TDClient:
 
         td_client = TDClient(
-            clients_id = self.client_id,
+            client_id = self.client_id,
             redirect_uri = self.redirect_uri,
             credentials_path = self.credentials_path
         )
